97ed44f98efe1dcac683a083a4a889b728a9593f
Prashant <aoiokido123@gmail.com>
2026-05-17 22:12:19 +0545

    update

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000'
+  origin: 'https://newsflow01.netlify.app'
 }));
 
 app.use(express.json());
